d94514d03824bd9819da549aec1cbe260e8d30b1
quinnj84 <jamesquinn1984@hotmail.co.uk>
2023-02-01 19:54:17 +0000

    latestcv

--- html folder/JQCV3.js
+++ html folder/JQCV3.js
@@ -1,4 +1,22 @@
-function myFunction() {
+function myFunction1() {
+    var x =document.getElementById("expandEdu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display= "none";
+    }
+}
+
+function myFunction2() {
+    var x =document.getElementById("expandProEx");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display= "none";
+    }
+}
+
+function myFunction3() {
     var x =document.getElementById("expandSkills");
     if (x.style.display === "none") {
         x.style.display = "block";
@@ -6,3 +24,20 @@ function myFunction() {
         x.style.display= "none";
     }
 }
+function myFunction4() {
+    var x =document.getElementById("expandForm");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display= "none";
+    }
+}
+
+function counterFunction() {
+    var counter = document.getElementById("count");
+    var count = 0;
+    count = parseInt(counter.innerHTML);
+    count = count + 1;
+    counter.innerHTML = count;
+}
+    window.onload = counterFunction;
--- html folder/Javaindex.js
+++ html folder/Javaindex.js
@@ -112,7 +112,9 @@
 let form, list, image, span
 window.addEventListener('load',()=>{
     form = document.querySelector("#form")
-    list = document.querySelector
+    list = document.querySelector("#list")
+    image = document.querySelector("#image")
+    span = document.querySelector("#username")
     
 
     form.addEventListener("submit",(event)=>{
@@ -123,11 +125,24 @@ window.addEventListener('load',()=>{
         console.log("formData", formData)
         const data = Object.fromEntries(formData)
         console.log("data", data)
+        const username = data.username
         fetch(`https://api.github.com/users/${data.username}`)
         .then((response)=>{
             return response.json()
         })
         .then((data)=>{
+            const {avatar_url, public_repos, followers} = data
+            image.setAttribute("src", avatar_url)
+            span.innerHTML = username
+            const li1 = document.createElement('li')
+            li1.innerHTML = `Public Repos: ${public_repos}`
+
+            const li2 = document.createElement('li')
+            li2.innerHTML = `Followers: ${followers}`
+
+            list.appendChild(li1)
+            list.appendChild(li2)
+
             console.log("response", data)
         })
     })
